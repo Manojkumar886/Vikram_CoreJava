c5a3ae85dca23ef20e563a424952585125ff606b
Manojkumar886 <maddymano434@gmail.com>
2023-08-05 12:32:10 +0530

    datatypes is completed

---
+++ src/main/java/core/DemoDatatypes.java
@@ -0,0 +1,15 @@
+package core;
+
+public class DemoDatatypes
+{
+    public static void main(String[] args)
+    {
+        String brandname="LAYS";
+        int price=10;
+        float quantity=88.7f;
+        char type='p';
+        long licno=1001206004000110l;
+        System.out.println("my brand name is :"+brandname+"\nproduct price :"+price+"\nthis product type is :"+type);
+
+    }
+}
